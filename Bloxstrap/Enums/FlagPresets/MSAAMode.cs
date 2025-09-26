28818a52238f5b3625c0e7961a9f993b677a4589
RealMeddsam <realferidjelalia@gmail.com>
2025-09-26 16:24:43 +0100

    added/keeping only allowed fflags

--- Bloxstrap/Enums/FlagPresets/MSAAMode.cs
+++ Bloxstrap/Enums/FlagPresets/MSAAMode.cs
@@ -4,12 +4,16 @@
     {
         [EnumName(FromTranslation = "Common.Default")]
         Default,
+
         [EnumName(StaticName = "1x")]
         x1,
+
         [EnumName(StaticName = "2x")]
         x2,
+
         [EnumName(StaticName = "4x")]
         x4,
+
         [EnumName(StaticName = "8x")]
         x8
     }
--- Bloxstrap/Enums/FlagPresets/RenderingMode.cs
+++ Bloxstrap/Enums/FlagPresets/RenderingMode.cs
@@ -5,10 +5,7 @@
         [EnumName(FromTranslation = "Common.Automatic")]
         Default,
         Vulkan,
-        [EnumName(StaticName = "Metal")]
-        Metal,
         OpenGL,
         D3D11,
-        D3D10,
     }
 }
